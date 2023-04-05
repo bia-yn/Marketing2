836565e33666cdb4b0b34f3b374b32539a9deb9d
Cynthia <114539349+bia-yn@users.noreply.github.com>
2023-04-04 21:16:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function exibirTexto() {
+    document.getElementById("meuTexto").innerHTML = "<h2>Copywriting é o processo de produção de textos persuasivos para ações de Marketing e Vendas, como o conteúdo de emails, sites, catálogos, anúncios e cartas de vendas. O profissional responsável pelo desenvolvimento do texto é conhecido como Copywriter.</h2>";
+}
